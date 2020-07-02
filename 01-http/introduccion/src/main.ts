b6d6fb9e4bf6f9b43b2627e73d60bb7d0d583bd7
Andrés Ruiz <franklin.ruiz@epn.edu.ec>
2020-07-02 16:01:04 -0500

    Modulo Http

--- 01-http/introduccion/src/main.ts
+++ 01-http/introduccion/src/main.ts
@@ -3,6 +3,8 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  console.log('Esperando por el puerto 3000');
+  //todas las configuraciones extras estan aqui arriba;
+  await app.listen(3001);
 }
 bootstrap();
